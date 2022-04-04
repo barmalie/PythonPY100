ed8e99a13ef6cb8b619a210f0052135f41495ede
Ruslan <6r0d5k1y@gmail.com>
2022-04-04 23:54:14 +0300

    Домашка 4 из 2 занятия готова

--- Занятие3/Домашнее_задание/task1/main.py
+++ Занятие3/Домашнее_задание/task1/main.py
@@ -4,13 +4,13 @@ def input_numbers():
 
     while True:
         input_num = int(input("введите новое чисоло: "))
-        if number < 0:
-            breake
+
+
         if 3<= input_num<= 13:
-             list_numbers.append(input_num)
+                list_numbers.append(input_num)
 
 
-    return list_numbers
+                return list_numbers
 
 if __name__ == "__main__":
     numbers = input_numbers()
