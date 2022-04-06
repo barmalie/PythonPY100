180abb62fbb48abce7ba6fe73a567e6cffb1419d
russos <ro0ne@mail.ru>
2022-04-06 20:53:14 +0300

    12_1 #4 тоже с подсказкой , проблемма с суммой четных чисел

--- Занятие4/Лабораторные_задания/task1_8/main.py
+++ Занятие4/Лабораторные_задания/task1_8/main.py
@@ -1,5 +1,6 @@
-def task():  # TODO записать решение в виде функции
-    ...
+def task(src_list: list) -> list:  # TODO записать решение в виде функции
+    #return [i**3 for i in src_list if i >= 0 ] #[64, 1000, 27, 512, 216, 729]
+    return [i**3 if i >0 else 0 for i in src_list]
 
 
 if __name__ == "__main__":
--- Занятие4/Лабораторные_задания/task2_1/main.py
+++ Занятие4/Лабораторные_задания/task2_1/main.py
@@ -1,17 +1,17 @@
 if __name__ == "__main__":
     number = 123456789
 
-    list_digits = ...  # TODO c помощью list comprehension получить список цифр числа
+    list_digits = [int(d) for d in str(number)]  # TODO c помощью list comprehension получить список цифр числа
     print(list_digits)
 
-    print(...)  # TODO найти сумму цифр числа
+    print(sum(list_digits))  # TODO найти сумму цифр числа
 
-    print(...)  # TODO найти сумму всех четных чисел
+    print(sum([d for d in list_digits if d%2 ==0]))  # TODO найти сумму всех четных чисел #немного не додумал где sum поставить
 
-    print(...)  # TODO найти количество цифр
+    print(len(list_digits))  # TODO найти количество цифр
 
-    print(...)  # TODO найти минимальную цифру
+    print(min(list_digits))  # TODO найти минимальную цифру
 
-    print(...)  # TODO все цифры стоящие на нечетных местах
+    print(list_digits[0::2])  # TODO все цифры стоящие на нечетных местах
 
-    print(...)  # TODO найти разность между первой и последней цифрой
+    print(list_digits[0]-list_digits[8])
