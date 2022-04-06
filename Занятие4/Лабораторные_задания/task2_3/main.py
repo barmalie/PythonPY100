0c0576c1d47b38a0d52c0645081180a18604657f
russos <ro0ne@mail.ru>
2022-04-06 21:20:00 +0300

    2_3 #4 не понятное

--- Занятие4/Лабораторные_задания/task2_3/main.py
+++ Занятие4/Лабораторные_задания/task2_3/main.py
@@ -1,7 +1,7 @@
-def task(num):  # TODO добавить аннотацию типов
-    ...  # TODO найти сумму цифр числа и понять двузначная ли она
-
-
+def task(num: int) -> bool:  # TODO добавить аннотацию типов
+    digits_list = [int(d) for d in str(num)]
+    sum_ = sum(digits_list)
+    join_num = "".join([str(d) for d in digits_list])
 if __name__ == "__main__":
     print(task(12))
     print(task(555))
